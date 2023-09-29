8c5fc7b5a74eb9f2c10bc05d45035909c963e684
Mathieu Larue <mlarue@mirego.com>
2023-09-29 10:01:52 -0400

    Preview

---
+++ sample/ios/ios/MainView.swift
@@ -0,0 +1,88 @@
+import SwiftUI
+import common
+
+struct MainView: View {
+    @State private var url = ""
+    @State private var key = ""
+    @State private var version = ""
+    @State private var language = ""
+    @State private var customDialog = false
+    @State private var viewData: KillswitchViewData? = nil
+    
+    var body: some View {
+        ZStack() {
+            VStack(spacing: 16) {
+                Image("icon")
+                    .resizable()
+                    .frame(width: 128, height: 128)
+                TextField(
+                    "Url",
+                    text: $url
+                )
+                TextField(
+                    "Key",
+                    text: $key
+                )
+                TextField(
+                    "Version",
+                    text: $version
+                )
+                TextField(
+                    "Language",
+                    text: $language
+                )
+                Toggle("Custom dialog", isOn: $customDialog)
+                Button("Engage") {
+                    Task {
+                        do {
+                            let viewData = try await IOSKillswitch().engage(key: key, version: version, language: language, url: url)
+                            
+                            class Listener : KillswitchListener {
+                                func onAlert() {
+                                    print("onAlert")
+                                }
+                                
+                                func onDialogShown() {
+                                    print("onDialogShown")
+                                }
+                                
+                                func onKill() {
+                                    print("onKill")
+                                }
+                                
+                                func onOk() {
+                                    print("onOk")
+                                }
+                            }
+                            
+                            if (customDialog) {
+                                self.viewData = viewData
+                            } else {
+                                IOSKillswitch().showDialog(viewData: viewData, listener: Listener())
+                            }
+                        }
+                        catch {
+                            print("Killswitch error: \(error)")
+                        }
+                    }
+                }
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+            .padding(32)
+            .blur(radius: viewData != nil ? 5 : 0)
+            
+            if let viewData = viewData {
+                CustomDialog(viewData: viewData) {
+                    self.viewData = nil
+                }
+            }
+        }
+        
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
--- sample/ios/ios/iOSApp.swift
+++ sample/ios/ios/iOSApp.swift
@@ -3,84 +3,9 @@ import common
 
 @main
 struct iOSApp: App {
-    @State private var url = ""
-    @State private var key = ""
-    @State private var version = ""
-    @State private var language = ""
-    @State private var customDialog = false
-    @State private var viewData: KillswitchViewData? = nil
-
     var body: some Scene {
         WindowGroup {
-            ZStack() {
-                VStack(spacing: 16) {
-                    Image("icon")
-                        .resizable()
-                        .frame(width: 128, height: 128)
-                    TextField(
-                        "Url",
-                        text: $url
-                    )
-                    TextField(
-                        "Key",
-                        text: $key
-                    )
-                    TextField(
-                        "Version",
-                        text: $version
-                    )
-                    TextField(
-                        "Language",
-                        text: $language
-                    )
-                    Toggle("Custom dialog", isOn: $customDialog)
-                    Button("Engage") {
-                        Task {
-                            do {
-                                let viewData = try await IOSKillswitch().engage(key: key, version: version, language: language, url: url)
-
-                                class Listener : KillswitchListener {
-                                    func onAlert() {
-                                        print("onAlert")
-                                    }
-
-                                    func onDialogShown() {
-                                        print("onDialogShown")
-                                    }
-
-                                    func onKill() {
-                                        print("onKill")
-                                    }
-
-                                    func onOk() {
-                                        print("onOk")
-                                    }
-
-
-                                }
-
-                                if (customDialog) {
-                                    self.viewData = viewData
-                                } else {
-                                    IOSKillswitch().showDialog(viewData: viewData, listener: Listener())
-                                }
-                            }
-                            catch {
-                                print("Killswitch error: \(error)")
-                            }
-                        }
-                    }
-                }
-                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
-                .padding(32)
-                .blur(radius: viewData != nil ? 5 : 0)
-
-                if let viewData = viewData {
-                    CustomDialog(viewData: viewData) {
-                        self.viewData = nil
-                    }
-                }
-            }
+            MainView()
         }
     }
 }
