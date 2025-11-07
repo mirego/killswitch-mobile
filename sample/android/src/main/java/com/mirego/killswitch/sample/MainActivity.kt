25ddc64eb79a52d53839937ddb44e4b1e4fcea7d
Mathieu Larue <mathieularue@MacBookPro-de-Mathieu.local>
2025-11-07 16:02:46 -0500

    Cleanup

--- sample/android/src/main/java/com/mirego/killswitch/sample/MainActivity.kt
+++ sample/android/src/main/java/com/mirego/killswitch/sample/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -41,6 +42,7 @@ import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
 
         setContent {
